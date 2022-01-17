cd65ab50d0673512b86436a4312351a0e3dd4ade
Nicholas Stein <Nicholas.Stein@vu.com>
2022-01-17 15:43:20 -0600

    casting as int

--- payCalculator.py
+++ payCalculator.py
@@ -4,7 +4,7 @@ def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
     rate = input('Enter Rate:')
-    gross_pay = int(hrs)*float(rate)
+    gross_pay = int(hrs)*int(rate)
     print(f'{gross_pay}')
     # end assignment
 
